557c77c40b4ada4c3f6f65a39ba1fce60cecaa90
natchapim <natchapim@rmutsvmail.com>
2020-11-03 18:26:19 +0700

    case study

---
+++ src/ExchangeCurrencyApp.java
@@ -0,0 +1,12 @@
+public class ExchangeCurrencyApp {
+
+    public static void main (String[] args){
+
+        ExchangeAPI ex = new ExchangeAPI();
+
+        // test connection with THB
+        if (ex.getConnection("THB")){
+            System.out.println(ex.getConversion_rates());
+        }
+    }
+}
